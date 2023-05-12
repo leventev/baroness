54d687f2aae298618cd8719de95b1e47d1774b1c
leventev <levente.vamos@protonmail.com>
2023-05-12 23:10:45 +0200

    Implement transfer instructions

--- src/emu/trans.rs
+++ src/emu/trans.rs
@@ -1,25 +1,67 @@
-use super::{AddressingMode, Emulator, instructions::Operand};
+use super::{instructions::Operand, Emulator};
 
 pub fn tax(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => {
+            emu.set_x(emu.regs.x);
+        }
+        _ => unreachable!(),
+    }
+
+    0
 }
 
 pub fn tay(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => {
+            emu.set_y(emu.regs.x);
+        }
+        _ => unreachable!(),
+    }
+
+    0
 }
 
 pub fn tsx(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => {
+            emu.set_x(emu.regs.sp);
+        }
+        _ => unreachable!(),
+    }
+
+    0
 }
 
 pub fn txa(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => {
+            emu.set_a(emu.regs.a);
+        }
+        _ => unreachable!(),
+    }
+
+    0
 }
 
 pub fn txs(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => {
+            emu.regs.sp = emu.regs.x;
+        }
+        _ => unreachable!(),
+    }
+
+    0
 }
 
 pub fn tya(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => {
+            emu.set_a(emu.regs.y);
+        }
+        _ => unreachable!(),
+    }
+
+    0
 }

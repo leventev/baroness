7e8c1ac8a647c978cc8fd5c871fc47c14267f128
leventev <levente.vamos@protonmail.com>
2023-05-12 23:29:46 +0200

    Implement branch instructions

--- src/emu/branch.rs
+++ src/emu/branch.rs
@@ -1,33 +1,37 @@
-use super::{Emulator, AddressingMode, instructions::Operand};
+use super::{Emulator, instructions::Operand, StatusRegister};
 
-pub fn bcc(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+macro_rules! branch_fn {
+    ($name: ident, $flag: expr, $cond: expr) => {
+        pub fn $name(emu: &mut Emulator, op: Operand) -> usize {
+            let mut extra_cycles = 0;
+
+            match op {
+                Operand::Relative(off) => {
+                    let final_addr = emu.regs.pc + off as u16;
+
+                    if emu.regs.flags.contains($flag) == $cond {
+                        extra_cycles += 1;
+
+                        if final_addr & 0xFF00 != emu.regs.pc & 0xFF00 {
+                            extra_cycles += 1;
+                        }
+
+                        emu.regs.pc = final_addr;
+                    }
+                },
+                _ => unreachable!()
+            }
+        
+            extra_cycles
+        }
+    };
 }
 
-pub fn bcs(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
-}
-
-pub fn beq(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
-}
-
-pub fn bmi(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
-}
-
-pub fn bne(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
-}
-
-pub fn bpl(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
-}
-
-pub fn bvc(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
-}
-
-pub fn bvs(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
-}
+branch_fn!(bcc, StatusRegister::CARRY, false);
+branch_fn!(bcs, StatusRegister::CARRY, true);
+branch_fn!(beq, StatusRegister::ZERO, true);
+branch_fn!(bne, StatusRegister::ZERO, false);
+branch_fn!(bmi, StatusRegister::NEGATIVE, true);
+branch_fn!(bpl, StatusRegister::NEGATIVE, false);
+branch_fn!(bvc, StatusRegister::OVERFLOW, false);
+branch_fn!(bvs, StatusRegister::OVERFLOW, true);

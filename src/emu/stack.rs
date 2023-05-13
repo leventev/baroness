2bdcd37ad248727d6b31d232c57447e377835bc4
leventev <levente.vamos@protonmail.com>
2023-05-13 22:55:02 +0200

    Implement stack instructions

--- src/emu/stack.rs
+++ src/emu/stack.rs
@@ -1,17 +1,43 @@
-use super::{Emulator, AddressingMode, instructions::Operand};
+use super::{instructions::Operand, AddressingMode, Emulator, StatusRegister};
 
 pub fn pha(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => emu.push_on_stack(emu.regs.a),
+        _ => unreachable!(),
+    }
+
+    0
 }
 
 pub fn php(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => emu.push_on_stack(emu.regs.flags.bits()),
+        _ => unreachable!(),
+    }
+
+    0
 }
 
 pub fn pla(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => {
+            let val = emu.pop_stack();
+            emu.set_a(val);
+        }
+        _ => unreachable!(),
+    }
+
+    0
 }
 
 pub fn plp(emu: &mut Emulator, op: Operand) -> usize {
-    todo!()
+    match op {
+        Operand::Implied => {
+            let val = emu.pop_stack();
+            emu.regs.flags = StatusRegister::from_bits(val).unwrap();
+        }
+        _ => unreachable!(),
+    }
+
+    0
 }
